dd46279399f2a8b4889f56d40b2d9653c6dd0f21
Anika <you@gmail.com>
2018-08-19 23:27:01 +0300

    Form first version
    Форма без эффектов и анимации

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+	prefixer = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
+	browserSync = require("browser-sync"),
+	reload = browserSync.reload;
+
+var path = {
+	build: {
+		html: 'build/',
+		js: 'build/js/',
+		css: 'build/css/',
+	},
+	src: {
+		html: 'src/*.html',
+		js: 'src/scripts/main.js',
+		style: 'src/styles/main.scss',
+	},
+	watch: {
+		html: 'src/*.html',
+		js: 'src/sctipts/**/*.js',
+		style: 'src/styles/**/*.scss',
+	},
+};
+gulp.task('html:build', function () {
+	gulp.src(path.src.html)
+		.pipe(gulp.dest(path.build.html))
+});
+gulp.task('style:build', function () {
+	gulp.src(path.src.style)
+		.pipe(sass())
+		.pipe(prefixer())
+		.pipe(gulp.dest(path.build.css))
+});
+gulp.task('js:build', function () {
+	gulp.src(path.src.js)
+
+		.pipe(gulp.dest(path.build.js))
+});
+gulp.task('browser-sync', function () { 
+	browserSync({ 
+		server: { 
+			baseDir: ''
+		},
+		notify: false 
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'style:build'], function () {
+	gulp.watch('src/styles/*.scss', ['style:build']);
+	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch('srk/scripts/*.js', browserSync.reload);
+});
+
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'style:build',
+]);
